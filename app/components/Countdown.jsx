444c498fdcedd1952201a82558635a3d5b8b175f
Jatto Ade <jattoade@gmail.com>
2017-05-07 19:40:48 +0100

    Add Navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+   render: function () {
+       return (
+         <h1>Countdown Tab</h1>
+       );
+   }
+});
+
+module.exports = Countdown;
--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -9,7 +9,7 @@ var Navigation = () => {
                     <ul className="menu">
                         <li className="menu-text">React TImer App</li>
                         <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
-                        <li><Link to="/" activeClassName="active-link">Countdown</Link></li>
+                        <li><Link to="/countdown" activeClassName="active-link">Countdown</Link></li>
                     </ul>
                 </div>
                 <div className="top-bar-right">
